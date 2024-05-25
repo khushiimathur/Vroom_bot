1aba93d207030bd39c87fa3dba6b0cd6e22edd0a
Sahil Narain <sahil.narain@gmail.com>
2024-05-25 16:55:18 +0000

    Added bot framework

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telethon.sync import TelegramClient
+from telethon.sync import TelegramClient, events
 from telethon.sessions import StringSession
 
 api_id = 26864957
@@ -6,14 +6,19 @@ api_hash = '0148f496077b4d88053cfc6e20ce0903'
 bot_token = '7162081466:AAEyI9UeOmAxKQh_pC2hkiPBz94PcZf_28k'
 
 # We have to manually call "start" if we want an explicit bot token
-bot = TelegramClient(StringSession(), api_id, api_hash).start(bot_token=bot_token)
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
+@bot.on(events.NewMessage(pattern='/start'))
+async def handleStart(event):
+    await event.respond('Hi, I am a bot!')
 
-async def main():
-    if bot.is_client_authorized():
-        print('Yes')
+# This is for every other message which doesn't match the above patterns
+@bot.on(events.NewMessage())
+async def handleMessage(event):
+    if event.message.message[0] == '/':
+        return # IF this is a Telegram command, then ignore the processing
 
-main()
+    # Change this with whatever processing you like.
+    await event.respond('Here is what you sent to me.\n\n' + str(event.message.message))
 
-# But then we can use the client instance as usual
-#with bot:
+bot.run_until_disconnected()
